d175c5ed6361756445c537dc109f0649ab850028
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-02-07 12:31:35 -0800

    feat(31325): building a console project to generate secrets

---
+++ PasswordsGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace GeradorSenhas
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.WriteLine("Welcome to Complex Passwirds Management !");
+			Console.WriteLine("Quantos caracteres você deseja que a senha tenha?");
+			int tamanhoSenha;
+			while (!int.TryParse(Console.ReadLine(), out tamanhoSenha) || tamanhoSenha <= 0)
+			{
+				Console.WriteLine("Por favor, insira um número inteiro positivo válido.");
+			}
+
+			string senha = GerarSenha(tamanhoSenha);
+			Console.WriteLine($"Sua senha gerada é: {senha}");
+
+			Console.WriteLine("\nPressione qualquer tecla para sair.");
+			Console.ReadKey();
+		}
+
+		static string GerarSenha(int tamanho)
+		{
+			const string caracteresPermitidos = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}[]<>?:;/";
+			Random rnd = new Random();
+			char[] senha = new char[tamanho];
+
+			for (int i = 0; i < tamanho; i++)
+			{
+				senha[i] = caracteresPermitidos[rnd.Next(caracteresPermitidos.Length)];
+			}
+
+			return new string(senha);
+		}
+	}
+}
