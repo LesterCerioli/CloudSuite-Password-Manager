fd20dc496156b814d360cd51276effb7e8a0de3d
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-06 17:02:14 -0300

    "feat(#1354): buidling Cross Cutting Infrastructure Project"

---
+++ PasswordGenerator.Infrastructure.CrossCutting/DependencyInjector/LoggingServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PasswordGenerator.Infrastructure.CrossCutting.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Infrastructure.CrossCutting.DependencyInjector
+{
+	public static class LoggingServiceCollectionExtension
+	{
+		public static IServiceCollection AddLogger(this IServiceCollection services)
+		{
+			var logger = LoggerManager.CreateLogger();
+			services.AddSingleton(logger);
+			return services;
+		}
+	}
+}
---
+++ PasswordGenerator.Infrastructure.CrossCutting/JsonSerializerSettingsGlobalConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Infrastructure.CrossCutting
+{
+	public static class JsonSerializerSettingsGlobalConfig
+	{
+		public static JsonSerializerOptions Settings { get; } = new JsonSerializerOptions
+		{
+			PropertyNameCaseInsensitive = true,
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			NumberHandling = JsonNumberHandling.AllowReadingFromString,
+			DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+			Converters = {
+
+				new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, allowIntegerValues: false) }
+		};
+	}
+}
