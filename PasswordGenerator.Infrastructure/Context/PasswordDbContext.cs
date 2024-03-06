a591385f95e811729de40148050d1673fb32c76e
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2024-03-06 14:59:47 -0300

    "feat(#1354): Mapping table with EF Core"

---
+++ PasswordGenerator.Infrastructure/Context/PasswordDbContext.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+using NetDevPack.Messaging;
+using PasswordGenerator.Domain.Models;
+using PasswordGenerator.Infrastructure.Mappings.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Infrastructure.Context
+{
+	public class PasswordDbContext : DbContext
+	{
+		public PasswordDbContext(DbContextOptions<PasswordDbContext> options)
+		{
+			ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+			ChangeTracker.AutoDetectChangesEnabled = false;
+		}
+
+		public DbSet<Password> Passwords { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Ignore<ValidationResult>();
+			modelBuilder.Ignore<Event>();
+
+			foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+					e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+				property.SetColumnType("varchar(100)");
+
+			modelBuilder.ApplyConfiguration(new PasswordEFCoreMapping());
+
+			modelBuilder.Entity<Password>(c =>
+			{
+				c.ToTable("Passwords");
+			});
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
---
+++ PasswordGenerator.Infrastructure/Mappings/EFCore/PasswordEFCoreMapping.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PasswordGenerator.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator.Infrastructure.Mappings.EFCore
+{
+	public class PasswordEFCoreMapping : IEntityTypeConfiguration<Password>
+	{
+		public void Configure(EntityTypeBuilder<Password> builder)
+		{
+			builder.HasKey(a => a.Id);
+
+			builder.Property(a => a.Id)
+				.HasColumnName("Id");
+
+			builder.Property(a => a.Senha)
+				.HasColumnName("senha")
+				.HasColumnType("varchar(24)");
+
+			builder.Property(a => a.CaracterNumber)
+				.HasColumnName("caracter")
+				.HasColumnType("varchar(24)")
+				.IsRequired();
+
+			builder.Property(a => a.CreatedOn)
+				.HasColumnName("createdon")
+				.HasColumnType("datetime2")
+				.HasConversion(
+					a => a.ToString(), 
+					v => DateTime.ParseExact(v, "yyyy-MM-dd HH:mm:ss", null));
+		}
+	}
+}
