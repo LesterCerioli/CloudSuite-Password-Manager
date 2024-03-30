98d81e83f6678300de2a5dbcba67c4da1b95d808
matrix <lesterlucasit@hotmail.com>
2024-03-30 17:15:18 -0300

    feat(#1407):building handlers on passwordGenerator

--- PasswordGenerator.Application/Handlers/Passwords/Requests/CheckPasswordExistsBySenhaRequest.cs
+++ PasswordGenerator.Application/Handlers/Passwords/Requests/CheckPasswordExistsBySenhaRequest.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using PasswordGenerator.Application.Handlers.Responses;
 
 namespace PasswordGenerator.Application.Handlers.Passwords.Requests
 {
-	public class CheckPasswordExistsBySenhaRequest
+	public class CheckPasswordExistsBySenhaRequest : IRequest<CheckPasswordExistsBySenhaResponse>
 	{
+		public Guid Id {get; private set;}
+
+		public string? Senha { get; set; }
+
+		public CheckPasswordExistsBySenhaRequest(string senha) 
+		{
+			Id = Guid.NewGuid();
+			Senha = senha;
+
+		}
+	
 	}
 }
--- PasswordGenerator.Application/Handlers/Passwords/Responses/CheckPasswordExistsBySenhaResponse.cs
+++ PasswordGenerator.Application/Handlers/Passwords/Responses/CheckPasswordExistsBySenhaResponse.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordGenerator.Application.Core;
+using FluentValidation.Results;
 
 namespace PasswordGenerator.Application.Handlers.Passwords.Responses
 {
-	public class CheckPasswordExistsBySenhaResponse
+	public class CheckPasswordExistsBySenhaResponse : Response
 	{
+		public Guid RequestId {get; private set;}
+
+		public bool Exists { get; set; }
+
+		public CheckPasswordExistsBySenhaResponse(Guid requestId, bool exists, ValidationResult result)
+		{
+			RequestId = requestId;
+            Exists = exists;
+            foreach (var item in result.Errors)
+            {
+                this.AddError(item.ErrorMessage);
+            }
+
+		}
+
+		public CheckPasswordExistsBySenhaResponse(Guid requestId, string failValidation)
+		{
+			RequestId = requestId;
+            Exists = false;
+            this.AddError(failValidation);
+		}
+
 	}
 }
+
+
