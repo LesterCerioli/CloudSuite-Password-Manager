f1b417a7ef7387c66ef1cfa08dd845ffefa6c23f
unknown <lesterlucasit@hotmail.com>
2024-04-01 21:54:19 -0300

    feat(#1407): Buildin Handlers to Password Generator context

--- PasswordGenerator.Application/Validations/CreatePasswordCommandValidation.cs
+++ PasswordGenerator.Application/Validations/CreatePasswordCommandValidation.cs
@@ -1,12 +1,32 @@
+using FluentValidation;
 using PasswordGenerator.Application.Handlers.Passwords;
 
 namespace PasswordGenerator.Application.Validations
 {
-    public class CreatePasswordCommandValidation
+    public class CreatePasswordCommandValidation : AbstractValidator<CreatePasswordCommand>
     {
-        internal object Validate(CreatePasswordCommand command)
+        public CreatePasswordCommandValidation()
         {
-            throw new NotImplementedException();
+            RuleFor(p => p.Senha)
+                .NotEmpty()
+                .WithMessage("The password field is required.")
+                .NotNull()
+                .WithMessage("The password field cannot be null.")
+                .MinimumLength(24)
+                .WithMessage("The password must be at least 24 characters long.");
+
+            RuleFor(p => p.CaracterNumber)
+                .NotNull()
+                .WithMessage("The character number field cannot be null.")
+                .GreaterThan(23)
+                .WithMessage("The character number must contain at least 24 characters.");
+
+            RuleFor(p => p.CreatedOn)
+                
+                .LessThanOrEqualTo(DateTimeOffset.Now)
+                .WithMessage("The created on date must be in the past or present.");
         }
     }
+
+
 }
